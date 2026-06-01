d43a74c9719fa38150daf5a8658a0421f0803960
Keivry <keivry@163.com>
2026-06-01 15:25:19 +0800

    chore: bump version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.1.2"
     }
 
     buildTypes {
@@ -20,7 +20,7 @@ android {
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
         debug {
